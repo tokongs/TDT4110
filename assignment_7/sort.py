6042d5887203df9303dbcaca453050636490747b
Tobias Kongsvik <tokongs98@gmail.com>
2018-10-22 12:51:29 +0200

    finished assignment 7

--- assignment_7/sort.py
+++ assignment_7/sort.py
@@ -10,10 +10,24 @@ def selectionSort(list):
         list.remove(max)
     return result
 
-def bubleSort(list):
-    while
+def bubbleSort(list):
+    while not isSorted(list):
+        for x in range(len(list)-1):
+            if(list[x] > list[x+1]):
+                tmp = list[x]
+                list[x] = list[x + 1]
+                list[x + 1] = tmp
+    return list
 
+def isSorted(list):
+    for x in range(1, len(list)):
+        if list[x] < list[x-1]:
+            return False
+    return True
     
 
 liste = [9,1,34,7,2,3,45,6,78,56,36,65,33,21,23,34,45,6]
 print(selectionSort(liste))
+
+liste = [9,1,34,7,2,3,45,6,78,56,36,65,33,21,23,34,45,6]
+print(bubbleSort(liste))
